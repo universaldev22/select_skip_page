5f9d18577418dcd879b21e03c9bd701a73987387
universaldev22 <waz92dev@gmail.com>
2025-05-14 15:07:41 -0400

    fixed theme for gap

--- src/containers/skip-selector/SkipSelector.tsx
+++ src/containers/skip-selector/SkipSelector.tsx
@@ -31,7 +31,7 @@ export const SkipSelector: React.FC = () => {
           p-4 md:p-8
         "
       >
-        <div className="max-w-7xl mx-auto">
+        <div className="max-w-7xl mx-auto pb-[25px]">
           <div className="text-center mb-12">
             <motion.h1
               initial={{ opacity: 0, y: 20 }}
